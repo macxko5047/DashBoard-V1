6475df66c55a0bbf3c824777894cfdfe67ebc779
macxko5047 <78531428+macxko5047@users.noreply.github.com>
2023-05-03 14:51:50 +0700

    EditAll

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,59 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-
+import axios from "axios";
 const prisma = new PrismaClient();
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ datas }: any) {
+  const [dataAll, setDataAll] = useState<any>([]);
+  console.log("dataAll", dataAll);
+  useEffect(() => {
+    const FetchData = async () => {
+      // let headersList = {
+      //   Authorization: "qwertyuiopasdw:password-tit-api-forfree",
+      //   apikey: "qwertyuiopasdw:password-tit-api-forfree",
+      // };
+
+      let reqOptions = {
+        url: "/api/posts",
+        method: "GET",
+        // headers: headersList,
+      };
+
+      let res = await axios.request(reqOptions);
+
+      if (res.data) {
+        // console.log(res.data);
+        setDataAll(res.data);
+      }
+    };
+    FetchData();
+    setInterval(FetchData, 10000);
+  }, []);
+
   return (
     <main>
       <div>
         <p>Get started by editing&nbsp;</p>
+        <p>Get API Prisma 1&nbsp;</p>
+
+        <ul>
+          {dataAll.map(
+            (res: {
+              Work_order_id: string;
+              PD_key: string;
+              Item_number: string;
+              OK_qty: number;
+            }) => (
+              <li key={res.PD_key}>
+                {res.PD_key} {res.Work_order_id} {res.Item_number} {res.OK_qty}
+              </li>
+            )
+          )}
+        </ul>
+
         <p>Get API Prisma 2.&nbsp;</p>
 
         <ul>
